84b47bbd2c63d08f3d214ca9fdd61d1d616fcaf4
Dharmendra Verma <sdharmendra8858@gmail.com>
2020-03-29 19:03:21 +0530

    header and footer added. App complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { Alpha2CodeService } from './shared/alpha2code.service';
-import { MapComponent } from './map/map.component';
 import { DataTransferService } from './shared/data-transfer.service';
 import { HeaderComponent } from './header/header.component';
-import { PieChartComponent } from './pie-chart/pie-chart.component';
 import { CovidDataService } from './covidData.service';
-import { InputDataComponent } from './input-data/input-data.component';
 import { HistoricalLineComponent } from './historical-line/historical-line.component';
 import { NewsLetterComponent } from './news-letter/news-letter.component';
 import { NewsLetterItemComponent } from './news-letter/news-letter-item/news-letter-item.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent,
     HeaderComponent,
-    PieChartComponent,
-    InputDataComponent,
     HistoricalLineComponent,
     NewsLetterComponent,
-    NewsLetterItemComponent
+    NewsLetterItemComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
